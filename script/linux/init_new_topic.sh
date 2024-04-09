814ef050edabad6bf1b01fd3d38cdc45d7121f53
Zheng Yang <snitchyang@qq.com>
2024-04-09 15:03:45 +0800

    linux script added

---
+++ script/linux/init_new_topic.sh
@@ -0,0 +1,1 @@
+./bin/kafka-topics.sh --create --topic storm-topic --bootstrap-server node1:9092
---
+++ script/linux/monitor_topic.sh
@@ -0,0 +1,1 @@
+./bin/kafka-console-consumer.sh --bootstrap-server node1:9092 --topic storm-topic  --from-beginning
